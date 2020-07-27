9593db75acaa84f432835dcb7baf74f121ae556c
ezart <ezart193@gmail.com>
2020-07-28 00:46:27 +0300

    Updates

---
+++ precache-manifest.23e5255fe39020e57eb4de07d507b2b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3745b51e200a4b651bf4e63fcd307cc",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "34c83f298b3647f3aa66",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eed7997836531ab5a6f7",
+    "url": "/myapp/static/js/2.9fca7837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.9fca7837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c83f298b3647f3aa66",
+    "url": "/myapp/static/js/main.3b71d709.chunk.js"
+  },
+  {
+    "revision": "0ec61fdc48078999ef6c",
+    "url": "/myapp/static/js/runtime-main.d0f71498.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
